80028ad5fa546b5e1226f0037329caf2c295c924
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-28 11:49:14 -0700

    adds machine index route and view

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using SillyFactory.Models;
+
+namespace SillyFactory
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+
+    
+  }
+}

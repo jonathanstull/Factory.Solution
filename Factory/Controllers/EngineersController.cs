8ae4eaba82f595d6d42244f0d7f2829aa8d7ae29
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-28 14:55:41 -0700

    allows machine assignment in edit route

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SillyFactory.Models;
 using System.Collections.Generic;
@@ -45,6 +46,7 @@ namespace SillyFactory.Controllers
     public ActionResult Edit(int id)
     {
       var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Model");
       return View(thisEngineer);
     }
 
@@ -71,5 +73,26 @@ namespace SillyFactory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    // public ActionResult AddMachine(int id)
+    // {
+    //   var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      
+    //   return View(thisEngineer);
+    // }
+
+    [HttpPost]
+    [ActionName("AddMachine")]
+    public ActionResult AddMachine(Engineer engineer, int MachineId)
+    {
+      if (MachineId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    
   }
 }

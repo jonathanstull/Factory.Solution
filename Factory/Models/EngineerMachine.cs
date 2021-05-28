cf54113ae08a02baeea4d9ba029c43eac71ceabd
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-28 10:00:26 -0700

    updates join table primary key to avoid migration errors

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -2,7 +2,7 @@ namespace SillyFactory.Models
 {
   public class EngineerMachine
   {
-    public int EngMachId { get; set; }   
+    public int Id { get; set; }   
     public int EngineerId { get; set; }
     public int MachineId { get; set; }
     public virtual Engineer Engineer { get; set; }
